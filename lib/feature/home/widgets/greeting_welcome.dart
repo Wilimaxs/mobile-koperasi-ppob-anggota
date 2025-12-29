997d87e75edac20c5955af912185e66e41ec87d2
wildantisna <wildan27370@gmail.com>
2025-12-29 23:15:32 +0700

    feat: Add greeting welcome widget

---
+++ lib/feature/home/widgets/greeting_welcome.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:ppob_koperasi_payment/utils/theme/app_color.dart';
+
+class GreetingWelcome extends StatelessWidget {
+  const GreetingWelcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: 'Welcome, ',
+                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              TextSpan(
+                text: 'Wilimaxs',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          width: 32,
+          height: 32,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: AppColors.white,
+            boxShadow: [
+              BoxShadow(
+                color: AppColors.text40,
+                blurRadius: 2,
+                offset: Offset(0, 1),
+              ),
+            ],
+          ),
+          child: IconButton(
+            padding: EdgeInsets.zero,
+            constraints: const BoxConstraints(),
+            onPressed: () {},
+            icon: const Icon(Icons.notifications_none_outlined, size: 24),
+          ),
+        ),
+      ],
+    );
+  }
+}
